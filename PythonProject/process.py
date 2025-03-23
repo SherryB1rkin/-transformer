dd043fa530d8e937e9dec3eee9fe8e0730c5b96a
k41se2 <127944719+SherryB1rkin@users.noreply.github.com>
2025-03-23 17:42:46 +0800

    Update process.py

--- PythonProject/process.py
+++ PythonProject/process.py
@@ -1,32 +1,40 @@
-import torch
 from config import config
+import torch
+from collections import Counter
+import jieba
 
 
 class DataProcessor:
-    def __init__(self, file_name, block_size=256):
+    def __init__(self, file_name):
         with open(file_name, 'r', encoding='GBK') as f:
             text = f.read()
 
-        self.chars = sorted(list(set(text)))
-        self.vocab_size = len(self.chars)
-        self.stoi = {ch: i for i, ch in enumerate(self.chars)}
-        self.itos = {i: ch for i, ch in enumerate(self.chars)}
+        words = list(jieba.cut(text))
+        word_counts = Counter(words)
+        vocab = [word for word, _ in word_counts.most_common(config.max_vocab - 1)]
+        vocab.append('<UNK>')
 
-        data = torch.tensor(self.encode(text), dtype=torch.long, device=config.device)
+        self.vocab_size = len(vocab)
+        self.stoi = {word: i for i, word in enumerate(vocab)}
+        self.itos = {i: word for i, word in enumerate(vocab)}
+
+        data = [self.stoi.get(w, self.stoi['<UNK>']) for w in words]
+        data = torch.tensor(data, dtype=torch.long, device=config.device)
+
         n = int(0.9 * len(data))
         self.train_data = data[:n]
         self.val_data = data[n:]
-        self.block_size = block_size
 
     def encode(self, s):
-        return [self.stoi[c] for c in s]
+        words = jieba.cut(s)
+        return [self.stoi.get(w, self.stoi['<UNK>']) for w in words]
 
     def decode(self, l):
         return ''.join([self.itos[i] for i in l])
 
     def get_batch(self, split):
         data = self.train_data if split == 'train' else self.val_data
-        start = torch.randint(len(data) - self.block_size, (config.batch_size,))
-        x = torch.stack([data[i:i + self.block_size].long() for i in start])
-        y = torch.stack([data[i + 1:i + self.block_size + 1].long() for i in start])
+        ix = torch.randint(len(data) - config.block_size, (config.batch_size,))
+        x = torch.stack([data[i:i + config.block_size] for i in ix])
+        y = torch.stack([data[i + 1:i + config.block_size + 1] for i in ix])
         return x.to(config.device), y.to(config.device)
